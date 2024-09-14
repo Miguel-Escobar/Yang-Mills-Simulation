cc231dea7d97bdc49aaf6be66a72dd185a6bd5dd
Sebastijo <sebastian.pincheira@ug.uchile.cl>
2024-09-14 02:06:34 -0300

    Bibliography added

--- U1_dist.jl
+++ U1_dist.jl
@@ -2,9 +2,10 @@ using ApproxFun
 using GLMakie
 using StaticArrays
 
-function yang_mills_boltzmann(first_face_neighbours::SVector{3, Float64}, second_face_neighbours::SVector{3, Float64}, β; n_samples::Int=1)
+function yang_mills_boltzmann(first_face_neighbours::SVector{3,Float64}, second_face_neighbours::SVector{3,Float64}, β; n_samples::Int=1)::Vector{Float64}
     pdf = Fun(θ -> exp.(β .* (cos.(sum(first_face_neighbours) .+ θ) .+ cos.(sum(second_face_neighbours) .+ θ))), Interval(0.0, 2π))
     samples = sample(pdf, n_samples)
+    println(typeof(samples))
     return samples
 end
 
