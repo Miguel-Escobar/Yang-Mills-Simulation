628b3b6c9159a9b31bc242f35d383b3517d53a31
Sebastijo <sebastian.pincheira@ug.uchile.cl>
2024-09-14 06:23:49 -0300

    Some comments

--- U1_dist.jl
+++ U1_dist.jl
@@ -1,15 +1,62 @@
 using ApproxFun
 using StaticArrays
 
-function yang_mills_boltzmann(first_face_neighbours::SVector{3,Float64}, second_face_neighbours::SVector{3,Float64}, β; n_samples::Int=1)::Vector{Float64}
-    pdf = Fun(θ -> exp.(β .* (cos.(sum(first_face_neighbours) .+ θ) .+ cos.(sum(second_face_neighbours) .+ θ))), Interval(0.0, 2π))
-    samples = sample(pdf, n_samples)
-    println(typeof(samples))
-    return samples
+
+"""
+yang_mills_boltzmann(first_face_neighbours::SVector{3,Float64}, second_face_neighbours::SVector{3,Float64}, β; n_samples::Int=1)::Vector{Float64}
+
+Computes the Yang-Mills Boltzmann distribution for a given set of face neighbours and a β value.
+
+# Arguments
+- `first_face_neighbours::SVector{3,Float64}`: The angles of the edges in the first face.
+- `second_face_neighbours::SVector{3,Float64}`: The angles of the edges in the second face.
+- `β::Float64`: The β value of the distribution.
+- `n_samples::Int=1`: The number of samples to draw from the distribution.
+
+# Returns
+- `samples`: The samples drawn from the distribution.
+"""
+function yang_mills_boltzmann(
+    first_face_neighbours::SVector{3, Float64},
+    second_face_neighbours::SVector{3, Float64},
+    β;
+    n_samples::Int = 1
+    )::Vector{Float64}
+
+	pdf = Fun(
+        θ -> exp.(β .* (cos.(sum(first_face_neighbours) .+ θ) .+ cos.(sum(second_face_neighbours) .+ θ))),
+        Interval(0.0, 2π)
+        )
+	samples = sample(pdf, n_samples)
+	println(typeof(samples))
+
+	return samples
 end
 
-function yang_mills_boltzmann(only_face_neighbours::SVector{3, Float64}, β; n_samples::Int=1)
-    pdf = Fun(θ -> exp.(β .* (cos.(sum(only_face_neighbours) .+ θ))), Interval(0.0, 2π))
-    samples = sample(pdf, n_samples)
-    return samples
+"""
+yang_mills_boltzmann(only_face_neighbours::SVector{3, Float64}, β; n_samples::Int=1)::Vector{Float64}
+
+Computes the Yang-Mills Boltzmann distribution for a given set of face neighbours and a β value.
+Used for faces with only one neighbouring face.
+
+# Arguments
+- `only_face_neighbours::SVector{3,Float64}`: The angles of the edges in the face.
+- `β::Float64`: The β value of the distribution.
+- `n_samples::Int=1`: The number of samples to draw from the distribution.
+
+# Returns
+- `samples`: The samples drawn from the distribution.
+"""
+function yang_mills_boltzmann(
+    only_face_neighbours::SVector{3, Float64},
+    β; n_samples::Int = 1
+    )::Vector{Float64}
+
+	pdf = Fun(
+        θ -> exp.(β .* (cos.(sum(only_face_neighbours) .+ θ))),
+        Interval(0.0, 2π)
+        )
+	samples = sample(pdf, n_samples)
+    
+	return samples
 end
--- z2_lattice.jl
+++ z2_lattice.jl
@@ -4,71 +4,100 @@ using CairoMakie # choosing a plotting backend
 include("U1_dist.jl")
 
 @agent struct Edge(GridAgent{2})
-    angle::Float64
+	angle::Float64
 end
 
 @agent struct Vertex(GridAgent{2})
 end
 
 @agent struct Face(GridAgent{2})
-    value::Float64
+	value::Float64
 end
 
 @multiagent Element(Edge, Vertex, Face) <: AbstractAgent
 
-function yang_mills_step!(agent, model)
-    β = model.β
-    if variant(agent) isa Edge
-        face_values = []
-        for neighbour in nearby_agents(agent, model, 1)
-            if variant(neighbour) isa Face
-                face_members = SVector{10,Float64}(
-                    [other_edge.angle for other_edge in nearby_agents(neighbour, model, 1) if other_edge != agent]
-                ) # Ineficiente pero no se me ocurre cómo pitearme el agente propio sin sacarlo primero de la lista.
-                push!(face_values, face_members)
-            end
-        end
-        agent.angle = yang_mills_boltzmann(face_values..., β)[1] # Aprovecho Multiple Dispatch. Slay!
-    end
+
+"""
+yang_mills_step!(agent, model)
+
+Updates the angle of an edge agent by sampling from a Boltzmann distribution
+conditional on the values of the edges in the faces that share the edge.
+
+# Arguments
+- `agent`: The edge agent to update.
+- `model`: The model containing the agent.
+
+# Returns
+- `nothing`
+"""
+function yang_mills_step!(agent, model)::Nothing
+	β = model.β
+	if variant(agent) isa Edge
+		face_values = []
+		for neighbour in nearby_agents(agent, model, 1)
+			if variant(neighbour) isa Face
+				face_members = SVector{10, Float64}(
+					[other_edge.angle for other_edge in nearby_agents(neighbour, model, 1) if other_edge != agent]
+				) # Ineficiente pero no se me ocurre cómo pitearme el agente propio sin sacarlo primero de la lista.
+				push!(face_values, face_members)
+			end
+		end
+		agent.angle = yang_mills_boltzmann(face_values..., β)[1] # Aprovecho Multiple Dispatch. Slay!
+	end
 end
 
+"""
+initialize_model(height::Int, width::Int, β::Float64)
+
+Initializes a model with a grid of size `height` x `width` with a given β value.
+This model is a 2D lattice with vertices, edges and faces. The vertices are initialized with no properties.
+The edges are initialized with an angle of 1.0. The faces are initialized with a value of 1.0.
+
+# Arguments
+- `height::Int`: The height of the grid.
+- `width::Int`: The width of the grid.
+- `β::Float64`: The β value of the model.
+
+# Returns
+- `model`: The initialized model.
+"""
 function initialize_model(height::Int, width::Int, β::Float64)
-    gridsize::Tuple{Int, Int} = (2 * height - 1, 2 * width - 1)
-    space = GridSpaceSingle(gridsize; periodic=false, metric=:manhattan)
-    properties = Dict(:β => β)
-    model = StandardABM(
-        Element,
-        space;
-        (agent_step!)=yang_mills_step!, properties,
-        container=Vector,
-        scheduler=Schedulers.ByKind((:Edge,)),
-    )
+	gridsize::Tuple{Int, Int} = (2 * height - 1, 2 * width - 1)
+	space = GridSpaceSingle(gridsize; periodic = false, metric = :manhattan)
+	properties = Dict(:β => β)
+	model = StandardABM(
+		Element,
+		space;
+		(agent_step!) = yang_mills_step!, properties,
+		container = Vector,
+		scheduler = Schedulers.ByKind((:Edge,)),
+	)
 
-    # We add the vertices
-    for (i, j) in product(2 .* (1:height) .- 1, 2 .* (1:width) .- 1)
-        agent = Element(Vertex(model, (i, j)))
-        add_agent_own_pos!(agent, model)
-    end
+	# We add the vertices
+	for (i, j) in product(2 .* (1:height) .- 1, 2 .* (1:width) .- 1)
+		agent = Element(Vertex(model, (i, j)))
+		add_agent_own_pos!(agent, model)
+	end
 
-    # We add the edges initialized with angle 1.0
-    edge_positions = Iterators.flatten(
-            (
-            product(2 .* (1: height) .- 1, 2 .* (1: width - 1)),
-            product(2 .* (1: height - 1), 2 .* (1: width) .- 1)
-            )
-        )
-    for (i, j) in edge_positions
-        agent = Element(Edge(model, (i, j), 1.0))
-        add_agent_own_pos!(agent, model)
-    end
+	# We add the edges initialized with angle 1.0
+	edge_positions = Iterators.flatten(
+		(
+		product(2 .* (1:height) .- 1, 2 .* (1:width-1)),
+		product(2 .* (1:height-1), 2 .* (1:width) .- 1),
+	)
+	)
+	for (i, j) in edge_positions
+		agent = Element(Edge(model, (i, j), 1.0))
+		add_agent_own_pos!(agent, model)
+	end
 
-    # We add the faces initialized with value 1.0
-    for (i, j) in product(2 .* (1: height - 1), 2 .* (1: width - 1))
-        agent = Element(Face(model, (i, j), 1.0))
-        add_agent_own_pos!(agent, model)
-    end
+	# We add the faces initialized with value 1.0
+	for (i, j) in product(2 .* (1:height-1), 2 .* (1:width-1))
+		agent = Element(Face(model, (i, j), 1.0))
+		add_agent_own_pos!(agent, model)
+	end
 
-    return model
+	return model
 end
 
 model = initialize_model(2, 2, 1.0)
