fc4fc6a5f5be5616b2090bc72e00b3c459f6e919
Miguel <77211681+Miguel-Escobar@users.noreply.github.com>
2024-09-14 02:48:38 -0300

    Yang Mills step agregada (no testeada)

--- U1_dist.jl
+++ U1_dist.jl
@@ -1,5 +1,4 @@
 using ApproxFun
-using GLMakie
 using StaticArrays
 
 function yang_mills_boltzmann(first_face_neighbours::SVector{3,Float64}, second_face_neighbours::SVector{3,Float64}, β; n_samples::Int=1)::Vector{Float64}
@@ -9,4 +8,8 @@ function yang_mills_boltzmann(first_face_neighbours::SVector{3,Float64}, second_
     return samples
 end
 
-hist(yang_mills_boltzmann(SVector(0.0, 0.0, 0.0), SVector(0.0, 0.0, 0.0), 1.0, n_samples=1000), bins=100)
+function yang_mills_boltzmann(only_face_neighbours::SVector{3, Float64}, β; n_samples::Int=1)
+    pdf = Fun(θ -> exp.(β .* (cos.(sum(only_face_neighbours) .+ θ))), Interval(0.0, 2π))
+    samples = sample(pdf, n_samples)
+    return samples
+end
--- z2_lattice.jl
+++ z2_lattice.jl
@@ -1,4 +1,5 @@
 using Agents
+include("U1_dist.jl")
 
 @agent struct Edge(GridAgent{2})
     angle::Float64
@@ -11,32 +12,35 @@ end
     value::Float64
 end
 
-function face_value(face)
-end
+@multiagent Element(Edge, Vertex, Face)
 
 function yang_mills_step!(agent, model)
+    β = model.β
+    if variant(agent) isa Edge
+        face_values = []
+        for neighbour in nearby_agents(agent, model, 1)
+            if variant(neighbour) isa Face
+                face_members = SVector{10, Float64}([other_edge.value for other_edge in nearby_agents(neighbour, model, 1) if other_edge != agent]) # Ineficiente pero no se me ocurre cómo pitearme el agente propio sin sacarlo primero de la lista.
+                push!(face_values, face_members)
+            end
+        end
+        agent.angle = yang_mills_boltzmann(face_values..., β)[1] # Aprovecho Multiple Dispatch. Slay.
+    end
 end
-
                 
-function initialize_model(height, width)
+function initialize_model(height, width, β)
     size = (2*height - 1, 2*width - 1)
     space = GridSpaceSingle(size; periodic = false, metric = :manhattan)
     total_vertices = height*width
     total_faces = (height - 1)*(width - 1)
     total_edges = (2*height - 1)*(2*width - 1) - total_vertices - total_faces
-    # model = StandardABM(
-    #     Edge,
-    #     space;
-    #     scheduler = Schedulers.Randomly(),
-    #     agent_step! = yang_mills_step!,
-    #     container= Vector
-    # )
-    # for (i, pos) in enumerate(all_positions(model))
-    #     orientation = rand(0:1)
-    #     value = rand() * 2π
-    #     agent = Edge(pos, orientation, value)
-    #     add_agent!(agent, model)
-    # end
-    # return model
+    model = StandardABM(
+        Element,
+        space;
+        agent_step! = yang_mills_step!,
+        container = Vector,
+        scheduler = Schedulers.ByKind((:Edge,))
+    )
+    return model
 end
 
