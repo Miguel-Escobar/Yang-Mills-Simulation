3dbf8086fbc70a0737a3bd6b51d55cc61d7586cc
Sebastijo <sebastian.pincheira@ug.uchile.cl>
2024-11-21 20:48:07 -0300

    Added images

--- __main__.jl
+++ __main__.jl
@@ -1,5 +1,8 @@
 using ProgressMeter
 using CairoMakie
+
+CairoMakie.activate!()
+
 plotFontsize = 30
 
 current_dir = @__DIR__
@@ -21,9 +24,9 @@ width = 10
 β = 10.0
 # interactive_exploration(height, width , β, group)
 
-group = "U1"
-height = 100
-width = 100
+group = "Z2"
+height = 10
+width = 10
 β = 0.1
 n_samples = 1000
 V_β = get_V_func14(width, height, β, group, n_samples)
@@ -53,40 +56,40 @@ imaginary_part_ratio = imaginary_part ./ x_values  # y/x for imaginary part
 norms_ratio = norms ./ x_values  # y/x for norms
 
 # Create the plots for the real part
-fig1 = Figure(resolution = (1500, 1000), fontsize = plotFontsize)
+fig1 = Figure(fontsize = plotFontsize)
 ax1 = Axis(fig1[1, 1], title = "Real Part", xlabel = "R", ylabel = "Real")
 lines!(ax1, 1:length(real_part), real_part, label = "Real Part")
 
-fig2 = Figure(resolution = (1500, 1000), fontsize = plotFontsize)
+fig2 = Figure(fontsize = plotFontsize)
 ax2 = Axis(fig2[1, 1], title = "Real Part / R", xlabel = "R", ylabel = "Real / R")
 lines!(ax2, 1:length(real_part_ratio), real_part_ratio, label = "Real Part / R")
 
 # Create the plots for the imaginary part
-fig3 = Figure(resolution = (1500, 1000), fontsize = plotFontsize)
+fig3 = Figure(fontsize = plotFontsize)
 ax3 = Axis(fig3[1, 1], title = "Imaginary Part", xlabel = "R", ylabel = "Imaginary")
 lines!(ax3, 1:length(imaginary_part), imaginary_part, label = "Imaginary Part")
 
-fig4 = Figure(resolution = (1500, 1000), fontsize = plotFontsize)
+fig4 = Figure(fontsize = plotFontsize)
 ax4 = Axis(fig4[1, 1], title = "Imaginary Part / R", xlabel = "R", ylabel = "Imaginary / R")
 lines!(ax4, 1:length(imaginary_part_ratio), imaginary_part_ratio, label = "Imaginary Part / R")
 
 # Create the plots for the norm
-fig5 = Figure(resolution = (1500, 1000), fontsize = plotFontsize)
+fig5 = Figure(fontsize = plotFontsize)
 ax5 = Axis(fig5[1, 1], title = "Norm", xlabel = "R", ylabel = "Norm")
 lines!(ax5, 1:length(norms), norms, label = "Norm")
 
-fig6 = Figure(resolution = (1500, 1000), fontsize = plotFontsize)
+fig6 = Figure(fontsize = plotFontsize)
 ax6 = Axis(fig6[1, 1], title = "Norm / R", xlabel = "R", ylabel = "Norm / R")
 lines!(ax6, 1:length(norms_ratio), norms_ratio, label = "Norm / R")
 
 # 7th plot: Colored scatter of the data (real part vs imaginary part) connected by a line
-fig7 = Figure(resolution = (1500, 1000), fontsize = plotFontsize)
+fig7 = Figure(fontsize = plotFontsize)
 ax7 = Axis(fig7[1, 1], title = "Colored Scatter of Data", xlabel = "Real Part", ylabel = "Imaginary Part")
 scatter!(ax7, real_part, imaginary_part, color = 1:length(real_part), colormap = :viridis)
 lines!(ax7, real_part, imaginary_part, color = 1:length(real_part), colormap = :viridis)
 
 # 8th plot: Colored scatter of the data divided by R (real part / R vs imaginary part / R) connected by a line
-fig8 = Figure(resolution = (1500, 1000), fontsize = plotFontsize)
+fig8 = Figure(fontsize = plotFontsize)
 ax8 = Axis(fig8[1, 1], title = "Colored Scatter of Data / R", xlabel = "Real Part / R", ylabel = "Imaginary Part / R")
 scatter!(ax8, real_part_ratio, imaginary_part_ratio, color = 1:length(real_part_ratio), colormap = :viridis)
 lines!(ax8, real_part_ratio, imaginary_part_ratio, color = 1:length(real_part_ratio), colormap = :viridis)
@@ -96,13 +99,13 @@ mkpath("data")
 mkpath("data/$(height) x $(width) x $(β) x $(group) x $(n_samples)")
 
 # Save the plots to the 'data' directory
-save("data/$(height) x $(width) x $(β) x $(group) x $(n_samples)/Real Part.png", fig1)
-save("data/$(height) x $(width) x $(β) x $(group) x $(n_samples)/Real Part div R.png", fig2)
-save("data/$(height) x $(width) x $(β) x $(group) x $(n_samples)/Img Part.png", fig3)
-save("data/$(height) x $(width) x $(β) x $(group) x $(n_samples)/Img Part div R.png", fig4)
-save("data/$(height) x $(width) x $(β) x $(group) x $(n_samples)/Norm.png", fig5)
-save("data/$(height) x $(width) x $(β) x $(group) x $(n_samples)/Norm div R.png", fig6)
-save("data/$(height) x $(width) x $(β) x $(group) x $(n_samples)/All.png", fig7)
-save("data/$(height) x $(width) x $(β) x $(group) x $(n_samples)/All div R.png", fig8)
+save("data/$(height) x $(width) x $(β) x $(group) x $(n_samples)/Real Part.svg", fig1)
+save("data/$(height) x $(width) x $(β) x $(group) x $(n_samples)/Real Part div R.svg", fig2)
+save("data/$(height) x $(width) x $(β) x $(group) x $(n_samples)/Img Part.svg", fig3)
+save("data/$(height) x $(width) x $(β) x $(group) x $(n_samples)/Img Part div R.svg", fig4)
+save("data/$(height) x $(width) x $(β) x $(group) x $(n_samples)/Norm.svg", fig5)
+save("data/$(height) x $(width) x $(β) x $(group) x $(n_samples)/Norm div R.svg", fig6)
+save("data/$(height) x $(width) x $(β) x $(group) x $(n_samples)/All.svg", fig7)
+save("data/$(height) x $(width) x $(β) x $(group) x $(n_samples)/All div R.svg", fig8)
 
 println("Plots saved to the 'data' directory.")
