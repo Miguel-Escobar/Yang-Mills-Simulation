837a769921c55eed2115efee78670c5585204475
Miguel <77211681+Miguel-Escobar@users.noreply.github.com>
2024-11-24 21:18:10 -0300

    profiler ultimo comentario

--- profiler.jl
+++ profiler.jl
@@ -16,4 +16,6 @@ model = initialize_model(width, height, β, group)
 Esto te va a mostrar lo que se llama un "Flame Graph" de lo que está tomando tiempo en tu código. Las secciones que están abajo de otra es porque están siendo llamadas por lo de arriba. El ancho es proporcional al tiempo que toma cada parte. Por ejemplo, si de una función salen 3 bloques, uno mucho más grande que los otros 2, puedo asegurarme que ahí es donde debiera optimizar.
 
 Los colores también significan cosas. Eso no se muy bien, pero debe estar documentado en profview. Tiene que ver con si Julia está teniendo que adivinar el tipo de dato que está trabajando o si no. Esto también es fácil de acelerar pues sólo le dices el tipo de dato y listo. Creo que lo más grave es cuando aparece gris.
+
+Otro dato: Task Done y cosas así parece que tienen que ver con multiprocessing. Eso los hace un poco más difíciles de estudiar. Hay que tener ojo ahí.
 """
